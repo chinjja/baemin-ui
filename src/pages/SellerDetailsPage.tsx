c3c3fbba40056971e02959d4ce466be62dbd85ce
chinjja <chinjja@gmail.com>
2021-08-10 15:03:01 +0900

    data grid

--- src/pages/SellerDetailsPage.tsx
+++ src/pages/SellerDetailsPage.tsx
@@ -1,18 +1,11 @@
 import React, { useEffect, useState } from "react";
-import { Button, makeStyles, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from "@material-ui/core";
+import { Button, makeStyles, Typography } from "@material-ui/core";
 import { useHistory, useLocation } from "react-router-dom";
 import { getProducts, Product, Seller } from "../baemin/Baemin"
 import SellerUi from "../components/SellerUi";
-
-const useStyles = makeStyles({
-    table: {
-        minWidth: 650,
-    },
-});
+import { DataGrid, GridColDef } from "@material-ui/data-grid";
 
 export default function SellerDetailsPage() {
-    const classes = useStyles();
-
     const history = useHistory();
     const location = useLocation();
     const seller = location.state as Seller;
@@ -23,36 +16,40 @@ export default function SellerDetailsPage() {
         .then(data => setProducts(data))
     }, [seller]);
     
+    const columns: GridColDef[] = [
+        { field: 'id', headerName: 'ID', hide: true },
+        { field: 'code', headerName: 'Code', flex: 1},
+        { field: 'title', headerName: 'Title', flex: 1 },
+        { field: 'description', headerName: 'Description', flex: 1 },
+        { field: 'price', headerName: 'Price', type: 'number', flex: 1 },
+        { field: 'quantity', headerName: 'Quantity', type: 'number', flex: 1 },
+    ]
+
+    const rows = products.map(row => {
+        return {
+            id: row.id,
+            code: row.info.code,
+            title: row.info.title,
+            description: row.info.description,
+            price: row.info.price,
+            quantity: row.info.quantity,
+        }
+    })
+
     return (
         <div>
             <Typography>Seller Details Page</Typography>
             <SellerUi seller={seller}/>
             <Typography>Product List</Typography>
-            <TableContainer component={Paper}>
-                <Table className={classes.table}>
-                    <TableHead>
-                        <TableRow>
-                            <TableCell>Code</TableCell>
-                            <TableCell>Title</TableCell>
-                            <TableCell>Description</TableCell>
-                            <TableCell>Price</TableCell>
-                            <TableCell>Quantity</TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {products.map(row => (
-                            <TableRow key={row.id}>
-                                <TableCell>{row.info.code}</TableCell>
-                                <TableCell>{row.info.title}</TableCell>
-                                <TableCell>{row.info.description}</TableCell>
-                                <TableCell>{row.info.price}</TableCell>
-                                <TableCell>{row.info.quantity}</TableCell>
-                            </TableRow>
-                        ))}
-                    </TableBody>
-                </Table>
-            </TableContainer>
-            
+            <div style={{height: 400, width: '100%'}}>
+                <DataGrid
+                    columns={columns}
+                    rows={rows}
+                    pageSize={5}
+                    checkboxSelection
+                    disableSelectionOnClick
+                    />
+            </div>
             <Button variant="outlined" onClick={e=>{history.push("/product/add", seller)}}>Add Product</Button>
         </div>
     );
