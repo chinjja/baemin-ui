b3aaf12d620b79ac6abcb1bab429b92d93461cbb
chinjja <chinjja@gmail.com>
2021-08-23 16:13:16 +0900

    read only

--- src/pages/AddressPage.tsx
+++ src/pages/AddressPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { useLocation } from "react-router-dom";
 import { Address } from "../baemin/Baemin";
 import { Box, Checkbox, Divider, Grid, TextField, Typography } from "@material-ui/core";
@@ -17,16 +17,16 @@ export default function AddressPage() {
             <Box my={2}>
                 <Grid container direction="column" spacing={1}>
                     <Grid item>
-                        <Checkbox defaultChecked={address.master} color="primary" readOnly/>
+                        <Checkbox checked={address.master} color="primary" readOnly/>
                     </Grid>
                     <Grid item>
-                        <TextField fullWidth label="Name" variant="outlined" defaultValue={address.name} InputProps={{readOnly: true}}></TextField>
+                        <TextField fullWidth label="Name" variant="outlined" value={address.name} InputProps={{readOnly: true}}></TextField>
                     </Grid>
                     <Grid item>
-                        <TextField fullWidth label="City" variant="outlined" defaultValue={address.city} InputProps={{readOnly: true}}></TextField>
+                        <TextField fullWidth label="City" variant="outlined" value={address.city} InputProps={{readOnly: true}}></TextField>
                     </Grid>
                     <Grid item>
-                        <TextField fullWidth label="Street" variant="outlined" defaultValue={address.street} InputProps={{readOnly: true}}></TextField>
+                        <TextField fullWidth label="Street" variant="outlined" value={address.street} InputProps={{readOnly: true}}></TextField>
                     </Grid>
                 </Grid>
             </Box>
